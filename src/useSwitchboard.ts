83fc205e6b12c349fd7ed0400c08861aded19260
Cory House <housecor@gmail.com>
2024-08-04 08:20:00 -0500

    inline

--- src/useSwitchboard.ts
+++ src/useSwitchboard.ts
@@ -97,6 +97,18 @@ export function useSwitchboard({
     return urlConfig;
   }
 
+  // Build a URL that contains a querystring key/value pair for each populated property in the provided config. By convention, each property name is mapped to the querystring's key.
+  function buildUrl<TDevToolsConfig>(
+    baseUrl: string,
+    config: Partial<TDevToolsConfig>
+  ) {
+    const params = new URLSearchParams();
+    for (const [key, value] of Object.entries(config)) {
+      params.append(key, JSON.stringify(value));
+    }
+    return baseUrl + "?" + params.toString();
+  }
+
   async function copyDevToolsSettingsUrlToClipboard() {
     const urlConfig = getChangedSettings();
     const url = buildUrl(window.location.href, {
