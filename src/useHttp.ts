4980fce26585f90b18b787ce8f8bf6bc35b2b0c9
Cory House <housecor@gmail.com>
2024-08-04 16:52:25 -0500

    pass settings

--- src/useHttp.ts
+++ src/useHttp.ts
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import { useDevToolsState } from "./useDevToolsState";
 import { SetupWorker, setupWorker } from "msw/browser";
-import { CustomResponse, HttpSettings } from "./http.types";
+import { CustomResponse, MswSettings } from "./http.types";
 
 export const httpDefaults = {
   delay: 0,
@@ -10,38 +10,33 @@ export const httpDefaults = {
 };
 
 /** Configure msw */
-export function useHttp<TCustomSettings>(
-  httpSettings: HttpSettings,
-  config: TCustomSettings
-) {
+export function useHttp(mswSettings: MswSettings) {
   // TODO: Move to URL?
   const [delay, setDelay, delayChanged] = useDevToolsState(
     "delay",
     httpDefaults.delay
   );
 
-  const configRef = useRef(config);
+  const configRef = useRef(mswSettings);
   const [isReady, setIsReady] = useState(false);
 
   // Passing an empty ref since merely invoking here to get the array so we can display the list of handlers in DevTools.
-  const requestHandlers = httpSettings.requestHandlers(useRef());
+  const requestHandlers = mswSettings.requestHandlers(useRef());
 
   const [customResponses, setCustomResponses] = useDevToolsState<
     CustomResponse[]
   >("customResponses", []);
 
-  // Store the config in a ref so the useEffect below that starts
-  // the worker runs only once, yet reads the latest config values
-  // as they change in the devtools.
+  // Store mswSettings in a ref so the useEffect below that starts the worker runs only once, yet reads the latest config value as they change in the devtools.
   useEffect(() => {
-    configRef.current = config;
-  }, [config]);
+    configRef.current = mswSettings;
+  }, [mswSettings]);
 
   useEffect(() => {
-    const worker = setupWorker(...httpSettings.requestHandlers(configRef));
+    const worker = setupWorker(...mswSettings.requestHandlers(configRef));
 
     const startWorker = async (worker: SetupWorker) => {
-      await worker.start(httpSettings.startOptions);
+      await worker.start(mswSettings.startOptions);
       setIsReady(true);
     };
 
