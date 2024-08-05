120f2acd8ca4924c6a06960579732022fcff9ae3
Cory House <housecor@gmail.com>
2024-08-04 20:06:49 -0500

    fix css build

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import { useSwitchboard } from "./useSwitchboard";
 import { Switchboard } from "./Switchboard";
 import { Http } from "./Http";
 import { useHttp } from "./useHttp";
+import "./switchboard.css";
 
 export { useSwitchboard, useDevToolsState, Switchboard, Http, useHttp };
