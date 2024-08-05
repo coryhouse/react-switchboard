fad794ed9ba3d46ad9293dc30da01d88b83e393b
Cory House <housecor@gmail.com>
2024-08-04 20:34:35 -0500

    fix

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig((options) => ({
-  entry: ["src/index.ts"],
+  entry: ["src/index.ts", "src/index.css"],
   clean: true,
   format: ["cjs", "esm"],
   dts: true,
