07f7d8fdfdfeea54b9266ab7e4d4dbee71893291
Cory House <housecor@gmail.com>
2024-08-07 07:19:44 -0500

    rename

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-import { useDevToolsState } from "./useDevToolsState";
+import { useSwitchboardState } from "./useSwitchboardState";
 import { useSwitchboard } from "./useSwitchboard";
 import { Switchboard } from "./Switchboard";
 import { Http } from "./Http";
 import { useHttp } from "./useHttp";
-import "./index.css";
 
-export { useSwitchboard, useDevToolsState, Switchboard, Http, useHttp };
+export { useSwitchboard, useSwitchboardState, Switchboard, Http, useHttp };
