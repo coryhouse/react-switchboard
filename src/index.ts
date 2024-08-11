e1c1ed50c12d9969ab4407fee2d0729305d8f4ea
Cory House <housecor@gmail.com>
2024-08-11 08:52:56 -0500

    Export types

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,25 @@ import { useSwitchboard } from "./useSwitchboard";
 import { Switchboard } from "./Switchboard";
 import { Http } from "./Http";
 import { useHttp } from "./useHttp";
+import { CustomResponse, MswSettings } from "./http.types";
+import {
+  Position,
+  Positions,
+  SwitchboardDefaults,
+  SwitchboardConfig,
+} from "./switchboard.types";
 
-export { useSwitchboard, useSwitchboardState, Http, useHttp };
+export {
+  useSwitchboard,
+  useSwitchboardState,
+  Http,
+  useHttp,
+  CustomResponse,
+  Position,
+  Positions,
+  SwitchboardDefaults,
+  SwitchboardConfig,
+  MswSettings,
+};
+
 export default Switchboard;
