e47f09f64a650f3c306d41eb57cd5d802d6b28a7
Cory House <housecor@gmail.com>
2024-08-12 10:19:31 -0600

    add demo

--- examples/vite-hello-world/src/main.tsx
+++ examples/vite-hello-world/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { lazy, StrictMode, Suspense } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "react-switchboard/dist/index.css";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+const Switchboard = lazy(() => import("react-switchboard"));
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {import.meta.env.DEV ? (
+      <Suspense fallback="Loading Switchboard...">
+        <Switchboard appSlot={<App />} />
+      </Suspense>
+    ) : (
+      <App />
+    )}
+  </StrictMode>
+);
